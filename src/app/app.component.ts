facc89683e4f26122ae621c0d6eda91bed655e9a
veselin0 <vesselin60@gmail.com>
2022-06-30 17:58:21 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage: string = '';
+
+  onInput(member: string) {
+    this.newMemberName = member;
+
+  }
+
+  addMember() {
+
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    this.errorMessage = "";
+
+  }
 }
