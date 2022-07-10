4a0f8b8f379cdfed4832c15dbca2caf51a291bab
veselin0 <vesselin60@gmail.com>
2022-07-10 17:46:37 +0300

    Added team generation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,16 @@ export class AppComponent {
   newMemberName = '';
   members: string[] = [];
   errorMessage: string = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
 
   onInput(member: string) {
     this.newMemberName = member;
 
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
   }
 
   addMember() {
@@ -26,5 +32,31 @@ export class AppComponent {
     this.newMemberName = "";
     this.errorMessage = "";
 
+  }
+
+  generateTeams() {
+    this.teams = [];
+    const allMembers = [...this.members];
+
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      return;
+    }
+
+    
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    console.log(this.teams);
   }
 }
