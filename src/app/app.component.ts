128b33b6549edeb600cbfce7732799f30a536ece
veselin0 <vesselin60@gmail.com>
2022-07-12 16:41:13 +0300

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,15 +39,23 @@ export class AppComponent {
     const allMembers = [...this.members];
 
     if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = "Invalid number of teams";
       return;
     }
 
-    
+    if (this.members.length < this.numberOfTeams) {
+      this.errorMessage = "Not enough members";
+      return;
+    }
+
+    this.errorMessage = "";
 
     while (allMembers.length) {
       for (let i = 0; i < this.numberOfTeams; i++) {
         const randomIndex = Math.floor(Math.random() * allMembers.length);
         const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) break;
 
         if (this.teams[i]) {
           this.teams[i].push(member);
@@ -57,6 +65,7 @@ export class AppComponent {
       }
     }
 
-    console.log(this.teams);
+    this.members = [];
+    this.numberOfTeams = "";
   }
 }
